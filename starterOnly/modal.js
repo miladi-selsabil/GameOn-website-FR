a33ebfd43973532c5dee2bc4f359516a473ad3a0
miladi-selsabil <selsabil.miladi@gmail.com>
2023-05-26 18:01:18 +0200

    projet terminer

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -30,6 +30,8 @@ function closek() {
   modalbg.style.display = "none";
 }
 //formulaire d'inscription
+
+//Declaration des variables
 const prenom = document.getElementById("first");
 const errorPrenom = document.querySelector(".error_first");
 
@@ -45,123 +47,194 @@ const errorBirthday = document.querySelector(".error_birthday");
 const tournoi = document.getElementById("quantity");
 const errorTournoi = document.querySelector(".error_number");
 
-const checkLocation = document.querySelectorAll("location");
-const errorCheckLocation = document.querySelectorAll(".error_location");
+const checkLocation = document.querySelectorAll("[name=location]");
+const errorCheckLocation = document.querySelector(".error_location");
 
-const checkboxCondition = document.querySelectorAll("#checkbox1");
+const checkboxCondition = document.querySelector("#checkbox1");
 const errorCheckboxCondition = document.querySelector(".error_condition");
 //click sur submt envoie du formulaire
+
+
 const form = document.getElementById("inscription");
+
+/*function pour envoi du formulaire si toute les conditions de la fonction submit sont remplis le formulaire
+est envoyer est un message de validation apparait */
+
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  if(conditionPrenom() && conditionEmail() && conditionEmail() && conditionTournoi() && conditionBirthday() && checkbox() && validCondition()){
-       modalConfirmation.style.display = "block";
-       form.style.display = "none";
-  }else{
-    alert("Merci de remplir correctement votre inscription.");
-    modalConfirmation.style.display = "none";
+  if (submitEvent()) {
+    modalConfirmation.style.display = "block";
+    form.style.display = "none";
   }
 });
 
-function conditionPrenom() {
-  const regex = new RegExp("^[A-Za-z.\\-\\_]{2,20}$");
-  prenom.addEventListener("blur", function (event) {
-    if (!regex.test(prenom.value)) {
-      errorPrenom.innerHTML = "le prenom n'est pas valide ";
-    } else {
-      errorPrenom.innerHTML = "";
-    }
-  });
-}
-conditionPrenom();
 
-function conditionNom() {
-  const regex = new RegExp("^[A-Za-z.\\-\\_]{2,20}$");
-  nom.addEventListener("blur", function (event) {
-    if (!regex.test(nom.value)) {
-      errorNom.innerHTML = "le nom n'est pas valide ";
-    } else {
-      errorNom.innerHTML = "";
-    }
-  });
-}
-conditionNom();
 
-function conditionEmail() {
-  const regexEmail = new RegExp(
-    "^[A-Za-z0-9.\\_]+[@]{1}[A-Za-z0-9.\\-\\_]+[.]{1}[a-z]{2,10}$"
-  );
-  email.addEventListener("blur", function (event) {
-    if (!regexEmail.test(email.value)) {
-      errorEmail.innerHTML = "le mail est incorrect ";
-    } else {
-      errorEmail.innerHTML = "";
-    }
-  });
-}
-conditionEmail();
-
-function conditionBirthday() {
-  const regexBirthday = new RegExp(
-    "/^([12]d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]d|3[01]))$/"
-  );
-  birthday.addEventListener("blur", function (event) {
-    if (!regexBirthday.test(birthday.value)) {
-      errorBirthday.innerHTML = "le date d'anniverssaire est incorrect ";
-    } else {
-      errorBirthday.innerHTML = "";
-    }
-  });
-}
-conditionBirthday();
-
-function conditionTournoi() {
-  const numbersValue = new RegExp("^[0-9]{1,2}$");
-  tournoi.addEventListener("blur", function (event) {
-    if (!numbersValue.test(tournoi.value)) {
-      errorTournoi.innerHTML = "le Nombre est incorrect ";
-    } else {
-      errorTournoi.innerHTML = "";
-    }
-  });
-}
-conditionTournoi();
-
+/*fonction pour les checkbox de la loaclisation ici une case doit etre cocher sinon message d'erreur  */
 function checkbox() {
+  console.log(checkLocation);
   for (let i = 0; i < checkLocation.length; i++) {
-    checkLocation.addEventListener("change", function (event) {
-      if (checkLocation[i].checked.value) {
-        errorCheckLocation.innerHTML = "";
-        return true;
-      } else {
-        errorCheckLocation.innerHTML = "Au moins une case doit être cochez";
-        return false;
-      }
-    });
+    if (checkLocation[i].checked) {
+      errorCheckLocation.innerHTML = "";
+      return true;
+    }
+  }
+  errorCheckLocation.innerHTML = "Au moins une case doit être cochez";
+  return false;
+}
+
+
+/*fonction pour la checkbox des conditions general la cose doit être obligatoirement cocher  */
+function validCondition(){
+    const checkboxCondition = document.querySelector("#checkbox1");
+    console.log(checkboxCondition)
+  if(checkboxCondition.checked){
+    errorCheckboxCondition.innerHTML="";
+    return true
+  }else{
+    errorCheckboxCondition.innerHTML="les conditions générales doivent être cochés";
+    return false;
   }
 }
-checkbox();
 
-function checkConditionControl() {
-  const checkboxCondition = document.querySelectorAll("#checkbox1");
-  let coche = false;
-  console.log(checkboxCondition);
-  checkboxCondition.forEach((label) => {
-    label.addEventListener("change", (event) => {
-      if (event.target.checked) {
-        coche = true;
-      }
-    });
+/*declaration des regex pour que les champs ne contiennent pas d'erreur */
+const regexName = new RegExp("^[A-Za-z.\\-\\_]{2,20}$");
+const regexEmail = new RegExp(
+  "^[A-Za-z0-9.\\_]+[@]{1}[A-Za-z0-9.\\-\\_]+[.]{1}[a-z]{2,10}$"
+);
+const regexNumber = new RegExp("^[0-9]{1,2}$");
+
+/*function submitEvent qui va permettre de verifier si les conditions sont respecter sinon elle renvoie un 
+message d'erreur au moment de l'envoie du formulaire  */
+function submitEvent(){
+  /**on commence par declarer tout les input et les div qui contiendront des messages d'erreur */
+  const fields = document.querySelectorAll(".text-control");
+  const fieldsError = document.querySelectorAll("error");
+  /*on declare formvalid qu'on intialise a true pour que si les condition sont valide tu m'envoie le formulaire */
+  let formValid = true;
+  /*on fait un tableau pour que comme le form valid est a true on initialise aucun message
+  d'erreur car on dit que de base il y a pas d'erreur donc aucun message
+  d'erreur ne s'affiche  */
+  Array.from(fieldsError).forEach((errorNode) =>{
+    errorNode.innerHTML="";
   });
-  return coche;
+
+  /*on recupere tout les champs input on va les recupere grace a 
+  leur id et on venir recuperer le parent des  input  */
+  Array.from(fields).forEach((nodeElement) =>{
+    const type = nodeElement.getAttribute("id");
+    const parent = nodeElement.parentNode;
+    const errorNode = parent.querySelector(".error");
+    switch (type) {
+      case "first":
+        /*On utilisera test() dès qu'on souhaite savoir si une partie d'une chaîne de caractères correspond à une expression rationnelle elle renvoie true ou false */
+        if (!regexName.test(nodeElement.value)) {
+          errorNode.innerHTML = "Le prenom est incorrect";
+          formValid = false;
+        }
+        break;
+      case "last":
+        if (!regexName.test(nodeElement.value)) {
+          errorNode.innerHTML = "Le nom est incorrect";
+          formValid = false;
+        }
+        break;
+      case "email":
+        if (!regexEmail.test(nodeElement.value)) {
+          errorNode.innerHTML = "l'email est incorrect";
+          formValid = false;
+        }
+        break;
+      case "quantity":
+        if (!regexNumber.test(nodeElement.value)) {
+          errorNode.innerHTML = "Nombre de tournoi invalide";
+          formValid = false;
+        }
+        break;
+      case "birthdate":
+        if (!nodeElement.value) {
+          errorNode.innerHTML = "Date de naissace invalide";
+          formValid = false;
+        }
+        break;
+    }
+  });
+
+  /*si les case sont cocher form valid = true  */
+  formValid = checkbox();
+  formValid = validCondition();
+
+  return formValid;
 }
 
-function validCondition() {
-  console.log("condition");
-  if (!checkConditionControl()) {
-    errorCheckboxCondition.innerHTML =
-      "les conditions génerales doivent être cocher";
-  } else {
-    errorCheckboxCondition.innerHTML = "";
-  }
+/*function initEvent qui va permettre de verifier si les conditions du champs sont respecter sans envoyer 
+le formulaire elle renvoie un message d'erreur au moment de changer et de passer au champs suivant 
+*/
+function initEvent() {
+  const fields = document.querySelectorAll(".text-control");
+  const fieldsError = document.querySelectorAll(".error");
+  let formValid = true;
+
+  Array.from(fieldsError).forEach((errorNode) => {
+    errorNode.innerHTML = "";
+  });
+  Array.from(fields).forEach((nodeElement) => {
+    const type = nodeElement.getAttribute("id");
+    const parent = nodeElement.parentNode;
+    const errorNode = parent.querySelector(".error");
+    switch (type) {
+      case "first":
+        nodeElement.addEventListener("change", function (event) {
+          if (!regexName.test(nodeElement.value)) {
+            errorNode.innerHTML = "Le prenom est incorrect";
+          } else {
+            errorNode.innerHTML = "";
+          }
+        });
+        break;
+      case "last":
+        nodeElement.addEventListener("change", function (event) {
+          if (!regexName.test(nodeElement.value)) {
+            errorNode.innerHTML = "Le nom est incorrect";
+          } else {
+            errorNode.innerHTML = "";
+          }
+        });
+        break;
+      case "email":
+        nodeElement.addEventListener("change", function (event) {
+          if (!regexEmail.test(nodeElement.value)) {
+            errorNode.innerHTML = "L'email est incorrect";
+          } else {
+            errorNode.innerHTML = "";
+          }
+        });
+        break;
+      case "quantity":
+        nodeElement.addEventListener("change", function (event) {
+          if (!regexNumber.test(nodeElement.value)) {
+            errorNode.innerHTML = "Le nombre de tournoi est incorrect";
+          } else {
+            errorNode.innerHTML = "";
+          }
+        });
+        break;
+      case "birthdate":
+        nodeElement.addEventListener("change", function (event) {
+          if (!nodeElement.value) {
+            errorNode.innerHTML = "L'annee d'anniverssaire est incorrect";
+          } else {
+            errorNode.innerHTML = "";
+          }
+        });
+        break;
+    }
+  });
+  //test les champs radio
+  formValid = checkbox();
+  formValid = validCondition();
+
+  return formValid;
 }
+
+initEvent();
